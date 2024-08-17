a3e8d796107a2b13e54be219117abbd8b7a80c4e
barcab96 <122807377+barcab96@users.noreply.github.com>
2024-08-17 17:14:08 +0200

    upload files commit

---
+++ includes/configuration.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/formula1dlbc/raw"
+processed_folder_path = "/mnt/formula1dlbc/processed"
+presentation_folder_path = "/mnt/formula1dlbc/presentation"
